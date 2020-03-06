b2d71367880aa717443eba246c93c34230be00e1
Yuval Lifshitz <yuvalif@yahoo.com>
2020-03-06 09:14:50 +0200

    in case of DRAWS=1 remove random shocks
    Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

--- c++/random_pools.cpp
+++ c++/random_pools.cpp
@@ -1,16 +1,24 @@
 #include <random>
 #include <boost/preprocessor/slot/counter.hpp>
+#include "const_parameters.h"
 
 // seed values: (BOOST_PP_COUNTER) should be incremented for each new function
 // new functions should be added at the end of the file, to keep the previouse ones consistent between runs
 // each distribution is using a mersenne_twister_engine seeded constant and different value
 
 double epsilon() {
+#if DRAWS > 1
     static std::mt19937 gen(BOOST_PP_COUNTER);
     static std::normal_distribution<> dis(0, 1);
     return dis(gen);
+#else 
+    // in case of single draw, there should be no noise
+    return 0.0;
+#endif
 }
 #include BOOST_PP_UPDATE_COUNTER()
+
+// in case of one draw there should be no noise
 
 int draw_3() {
     static std::mt19937 gen(BOOST_PP_COUNTER);
