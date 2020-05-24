143d3af3aae439d343cbec8265000067ea03e8d4
Yuval Lifshitz <yuvalif@yahoo.com>
2020-05-24 12:16:10 +0300

    fix wage unit tests
    Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

--- c++/calculate_wage_test.h
+++ c++/calculate_wage_test.h
@@ -4,88 +4,98 @@
 #include "draw_wife.h"
 #include "parameters.h"
 #include "load_files.h"
+#include "cpp-text-table/TextTable.h"
+#include "to_string.h"
 
 const auto p = load_parameters("../../init_parameters.txt", "../../tax_brackets.out", "../../deductions_exemptions.out", "../../wives.out", "../../husbands");
 
+void set_school_group(Husband& h, int school_group) {
+    if (school_group == 0) {
+        h.H_HSD = 1;
+    } else if (school_group == 1) {
+        h.H_HSG = 1;
+    } else if (school_group == 2) {
+        h.H_SC = 1;
+    } else if (school_group == 3) {
+        h.H_CG = 1;
+    } else if (school_group == 4) {
+        h.H_PC = 1;
+    }
+    assert(false);
+}
+
+void set_school_group(Wife& w, int school_group) {
+    if (school_group == 0) {
+        assert(false);
+    } else if (school_group == 1) {
+        w.HSG = 1;
+    } else if (school_group == 2) {
+        w.SC = 1;
+    } else if (school_group == 3) {
+        w.CG = 1;
+    } else if (school_group == 4) {
+        w.PC = 1;
+    }
+    assert(false);
+}
+
+static const auto epsilon_for_test = 0.0;
+static const auto no_wage_prob = 0.0;
+
 class calculate_wage_suite : public CxxTest::TestSuite {
 public:
+    void test_husband() {
+        TextTable table(' ');
+        table.add("Schooling");
+        table.add("Experience");
+        table.add("Ability");
+        table.add("Wage");
+        table.endOfRow();
 
-    void loop_over_all_husband_values(Husband husband, double min_wage, double max_wage) {
-        for (auto HE : exp_vector) {
-            for (auto ability : normal_arr) {
-                husband.HE = HE*p.sigma[4];
-                husband.ability_h_value = ability;
-                const auto wage = calculate_wage_h(p, husband, 1.0);
-                TS_ASSERT_LESS_THAN(wage, max_wage);
-                TS_ASSERT_LESS_THAN(min_wage, wage);
+        for (auto school_group : SCHOOL_H_VALUES) {
+            Husband husband;
+            set_school_group(husband, school_group);
+            for (auto ex : exp_vector) {
+                for (auto ability : normal_arr) {
+                    husband.HE = ex;
+                    husband.ability_h_value = ability*p.sigma[3];
+                    const auto wage = calculate_wage_h(p, husband, epsilon_for_test);
+                    table.add(std::to_string(school_group));
+                    table.add(std::to_string(ex));
+                    table.add(to_string_with_precision(ability, 3));
+                    table.add(to_string_with_precision(wage, 0));
+                    table.endOfRow();
+                }
             }
         }
+        std::cout  << std::endl << table << std::endl;
     }
 
-    void loop_over_all_wife_values(Wife wife, double min_wage, double max_wage) {
-        for (auto WE : exp_vector) {
-            for (auto ability : normal_arr) {
-                wife.WE = WE*p.sigma[3];
-                wife.ability_w_value = ability;
-                const auto wage = calculate_wage_w(p, wife, 0.0, 1.0);
-                TS_ASSERT_LESS_THAN(wage, max_wage);
-                TS_ASSERT_LESS_THAN(min_wage, wage);
+    void test_wife() {
+        TextTable table(' ');
+        table.add("Schooling");
+        table.add("Experience");
+        table.add("Ability");
+        table.add("Wage");
+        table.endOfRow();
+
+        for (auto school_group : SCHOOL_W_VALUES) {
+            Wife wife;
+            set_school_group(wife, school_group);
+            for (auto ex : exp_vector) {
+                for (auto ability : normal_arr) {
+                    wife.WE = ex;
+                    wife.ability_w_value = ability*p.sigma[3];
+                    const auto wage = calculate_wage_w(p, wife, no_wage_prob, epsilon_for_test);
+                    table.add(std::to_string(school_group));
+                    table.add(std::to_string(ex));
+                    table.add(to_string_with_precision(ability, 3));
+                    table.add(to_string_with_precision(wage, 0));
+                    table.endOfRow();
+                }
             }
         }
-    }
-
-	void test_husband_hsd() {
-        Husband husband;
-        husband.H_HSD = 1;
-        loop_over_all_husband_values(husband, 10000.0, 200000.0);
-    }
-
-	void test_husband_hsg() {
-        Husband husband;
-        husband.H_HSG = 1;
-        loop_over_all_husband_values(husband, 10000.0, 300000.0);
-    }
-
-	void test_husband_pc() {
-        Husband husband;
-        husband.H_PC = 1;
-        loop_over_all_husband_values(husband, 10000.0, 700000.0);
-    }
-
-	void test_husband_cg() {
-        Husband husband;
-        husband.H_CG = 1;
-        loop_over_all_husband_values(husband, 10000.0, 500000.0);
-    }
-
-	void test_husband_sc() {
-        Husband husband;
-        husband.H_SC = 1;
-        loop_over_all_husband_values(husband, 10000.0, 400000.0);
-    }
-
-	void test_wife_hsg() {
-        Wife wife;
-        wife.HSG = 1;
-        loop_over_all_wife_values(wife, 5000.0, 200000.0);
-    }
-
-	void test_wife_pc() {
-        Wife wife;
-        wife.PC = 1;
-        loop_over_all_wife_values(wife, 10000.0, 400000.0);
-    }
-	
-    void test_wife_cg() {
-        Wife wife;
-        wife.CG = 1;
-        loop_over_all_wife_values(wife, 10000.0, 300000.0);
-    }
-    
-    void test_wife_sc() {
-        Wife wife;
-        wife.SC = 1;
-        loop_over_all_wife_values(wife, 10000.0, 200000.0);
+        std::cout  << std::endl << table << std::endl;
     }
 };
 
