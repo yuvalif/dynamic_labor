e2f1aa08397c449bd5741b33719428a7684d8c26
Yuval Lifshitz <ylifshit@redhat.com>
2021-03-16 22:51:54 +0200

    fix bp test

--- c++/nash_test.h
+++ c++/nash_test.h
@@ -1,38 +1,55 @@
 #include <cxxtest/TestSuite.h>
 #include <iostream>
-#include <fstream>
 #include "nash.h"
+#include "draw_wife.h"
+#include "draw_husband.h"
 
 #include "parameters.h"
 #include "load_files.h"
 #include "calculate_utility.h"
 
 const auto p = load_parameters("../../init_parameters.txt", "../../tax_brackets.out", "../../deductions_exemptions.out", "../../wives.out", "../../husbands");
+const auto empty_emax = make_emax();
 
 class nash_suite : public CxxTest::TestSuite {
 public:
 	void test_nash() {
-        std::ifstream file("./utility.txt");
-        if (!file.is_open()) {
-            throw std::runtime_error("failed to open: utility.txt");
-        }
-        while (file.good()) {
-            Utility utility;
-            for (auto& u : utility.U_W_S) {
-                file >> u;
+    double bp = 0.5;
+    for (auto school_group : SCHOOL_W_VALUES) {
+        for (auto t = 0; t < T_MAX-1; ++t) {
+            Husband husband;
+            if (!update_husband_schooling(school_group, t, husband)) continue;
+            Wife wife;
+            if (!update_wife_schooling(school_group, t, wife)) continue;
+            for (auto h_exp_i : EXP_H_VALUES) { 
+                husband.HE = exp_vector[h_exp_i];
+                for (auto ability_hi : ABILITY_VALUES) {   
+                    // husband ability - high, medium, low
+                    update_ability(p, ability_hi, husband);
+                    for (auto wage_w = 0.0; wage_w < 400000.0; wage_w += 80000.0) {
+                        for (auto wage_h = 10000.0; wage_h < 700000.0; wage_h += 80000.0) {
+                            for (auto kids : KIDS_VALUES) {
+                                const auto u = calculate_utility(p, empty_emax, empty_emax, 
+                                        kids, 
+                                        wage_h,
+                                        wage_w,
+                                        1,                      // has partner 
+                                        MARRIED,
+                                        wife, 
+                                        husband,
+                                        t,
+                                        0.5,                        // BP 
+                                        false);                     // not single men
+                                std::cout << "****************************" << std::endl;
+                                std::cout << to_string(u);
+                                std::cout << "BP: " << nash(p, u, bp) << std::endl;
+                            }
+                        }
+                    }
+                }
             }
-            file >> utility.U_H_S;
-            for (auto& u : utility.U_W) {
-                file >> u;
-            }
-            for (auto& u : utility.U_H) {
-                file >> u;
-            }
-            double bp = 0.5;
-            std::cout << "****************************" << std::endl;
-            std::cout << to_string(utility);
-            std::cout << "BP: " << nash(p, utility, bp) << std::endl;
         }
     }
+  }
 };
 
